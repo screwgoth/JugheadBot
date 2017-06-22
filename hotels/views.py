790dea6252a97a60a487605596669618b281162d
screwgoth <raseelbhagat@gmail.com>
2017-06-22 23:08:42 +0530

    Removed recipient_id and PAGE_ACCESS_TOKEN hardcodings

--- hotels/views.py
+++ hotels/views.py
@@ -54,8 +54,8 @@ def handle_webhook(request):
                     PAGE_ACCESS_TOKEN=os.environ['PAGE_ACCESS_TOKEN']
                     print (PAGE_ACCESS_TOKEN)
                     headers={"Content-Type":"application/json"}
-                    params={"access_token":"EAAODm4aVRGkBAMxC39nLEOJJrSlMEL4nqiLRZBzMGjoC7HeXJy59ZAErZBB9jtE0T9b93QKnmn7AVhSEq9f204w8KaVYzbUgUiz8ElflvOO2vq6LNXIle5umJv8shoeNmye345z8za26xsZAUblVacC0yBRYwzZBoGZArQS4HykwZDZD"}
-                    data = json.dumps({"recipient":{"id":"1144124542355606"}, "message":{"text":"Wassup Yo"}})
+                    params={"access_token":PAGE_ACCESS_TOKEN}
+                    data = json.dumps({"recipient":{"id":recipient_id}, "message":{"text":"Wassup Yo"}})
 
                     status = requests.post(facebook_url,params=params,headers=headers,data=data)
                     print (status.status_code)

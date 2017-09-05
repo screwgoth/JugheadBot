50355af730e5532e6e49e13dbcc664fa724b32e8
Raseel Bhagat <raseel.bhagat@zymr.com>
2017-09-05 15:55:44 +0530

    Removed all the commented out lines

--- hotels/views.py
+++ hotels/views.py
@@ -12,20 +12,15 @@ def get_hotel_info(request):
     if request.method == 'GET':
         print (request.body)
         body_unicode = request.body.decode('utf-8')
-        #body = json.loads(body_unicode)
-        #print (body)
         zomato_url="https://developers.zomato.com/api/v2.1/categories"
         headers={"Accept":"applicaiton/json",
         "user-key": "b0fcc8e574f96ad3e80be23d898aa861"}
         resp=requests.get(zomato_url,headers=headers)
         print (resp.content)
-        #print (resp.status_code)
         jresp=json.loads(resp.text)
-        #print (resp.text)
         print (jresp)
         return Response(jresp)
     if request.method == 'POST':
-        #https://developers.zomato.com/api/v2.1/search?entity_id=5&entity_type=city&count=5&sort=rating&order=desc
         print (request.body)
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
@@ -34,7 +29,6 @@ def get_hotel_info(request):
         print (city)
         headers={"Accept":"applicaiton/json",
         "user-key": "b0fcc8e574f96ad3e80be23d898aa861"}
-        #eid=5
         search_url = "https://developers.zomato.com/api/v2.1/locations?query="+city
         search_resp=requests.get(search_url,headers=headers)
         search_resp_dict=json.loads(search_resp.text)
@@ -51,18 +45,8 @@ def get_hotel_info(request):
         restaurants = (resp_dict['restaurants'])
         disp_cat = ""
         for restaurant in restaurants:
-            #print (restaurant)
             name = restaurant['restaurant']['name']
-            #disp_cat += "\n".join(name)
             disp_cat += name + "\n "
-        # for item in resp_dict:
-        #     cat_list = resp_dict[item]
-        #     print (cat_list)
-        #     disp_cat = ""
-        #     for cats in cat_list:
-        #         categ = (cats['categories']['name'])
-        #         disp_cat += categ + ' '
-        #         print (disp_cat)
         tempresp= {
             "speech" : disp_cat,
             "displayText" : disp_cat,

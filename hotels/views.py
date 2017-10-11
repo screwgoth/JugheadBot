a4ed094c2ab94383e9f2b059a3d9fe8166f4ec07
Raseel Bhagat <raseel.bhagat@zymr.com>
2017-10-11 14:32:45 +0530

    Updated help message and handling of postbacls

--- hotels/views.py
+++ hotels/views.py
@@ -47,28 +47,30 @@ def get_hotel_info(request):
                     fb_userId=str(fb.sender_id)
                 )
                 fb_rating.save_to_db()
-                if "NEW_USER_STARTED" in body['originalRequest']['data']['postback']['payload']:
-                    fb.independantTextMessage(fb.sender_id, "Hey there, Foodie !!! I'm JugheadBot, your friendly neighbourhood Restaurant finding Bot")
-                    sleep(1)
-                    fb.independantTextMessage(fb.sender_id, "You can ask me following questions:")
-                    fb.independantTextMessage(fb.sender_id, "\"Which are the best Restaurants in Kothrud, Pune\"")
-                    sleep(1)
-                    fb.independantTextMessage(fb.sender_id, "\"Which are the best Chinese Restaurants in Dadar, Mumbai\"")
-                    sleep(1)
-                    fb.independantTextMessage(fb.sender_id, "\"What is the review of Blue Nile in Camp Area, Pune\"")
-                    sleep(1)
-                elif "HELP_TEXT" in body['originalRequest']['data']['postback']['payload']:
-                    fb.independantTextMessage(fb.sender_id, "Currently, I understand only the following 3 types of questions")
-                    fb.independantTextMessage(fb.sender_id, "\"Which are the best Restaurants in Kothrud, Pune\"")
-                    sleep(1)
-                    fb.independantTextMessage(fb.sender_id, "\"Which are the best Chinese Restaurants in Dadar, Mumbai\"")
-                    sleep(1)
-                    fb.independantTextMessage(fb.sender_id, "\"What is the review of Blue Nile in Camp Area, Pune\"")
-                    sleep(1)
-                    fb,independantTextMessage(fb.sender_id, "And PLEASE remember to specify the Area AND City. For example: \"Manhattan, New York\" or \"Dadar, Mumbai\"")
-                    sleep(1)
-                else:
-                    fb.independantTextMessage(fb.sender_id, "Thanks !! I'll let Raseel know how much you liked me !!")
+                postback = body['originalRequest']['data']['postback']['payload']
+                handle_postback(postback, fb)
+                # if "NEW_USER_STARTED" in body['originalRequest']['data']['postback']['payload']:
+                #     fb.independantTextMessage(fb.sender_id, "Hey there, Foodie !!! I'm JugheadBot, your friendly neighbourhood Restaurant finding Bot")
+                #     sleep(1)
+                #     fb.independantTextMessage(fb.sender_id, "You can ask me following questions:")
+                #     fb.independantTextMessage(fb.sender_id, "\"Which are the best Restaurants in Kothrud, Pune\"")
+                #     sleep(1)
+                #     fb.independantTextMessage(fb.sender_id, "\"Which are the best Chinese Restaurants in Dadar, Mumbai\"")
+                #     sleep(1)
+                #     fb.independantTextMessage(fb.sender_id, "\"What is the review of Blue Nile in Camp Area, Pune\"")
+                #     sleep(1)
+                # elif "HELP_TEXT" in body['originalRequest']['data']['postback']['payload']:
+                #     fb.independantTextMessage(fb.sender_id, "Currently, I understand only the following 3 types of questions")
+                #     fb.independantTextMessage(fb.sender_id, "\"Which are the best Restaurants in Kothrud, Pune\"")
+                #     sleep(1)
+                #     fb.independantTextMessage(fb.sender_id, "\"Which are the best Chinese Restaurants in Dadar, Mumbai\"")
+                #     sleep(1)
+                #     fb.independantTextMessage(fb.sender_id, "\"What is the review of Blue Nile in Camp Area, Pune\"")
+                #     sleep(1)
+                #     fb,independantTextMessage(fb.sender_id, "And PLEASE remember to specify the Area AND City. For example: \"Manhattan, New York\" or \"Dadar, Mumbai\"")
+                #     sleep(1)
+                # else:
+                #     fb.independantTextMessage(fb.sender_id, "Thanks !! I'll let Raseel know how much you liked me !!")
                 return Response("{}")
         except:
                 # Not a Postback, so continue
@@ -117,3 +119,38 @@ def get_hotel_info(request):
         }
         print(response)
         return Response(response)
+
+
+def handle_postback(postback, fb):
+    """
+    Handle the postback
+    """
+    if "NEW_USER_STARTED" in postback:
+        greeting_message = "Hello " + fb.userInfo['first_name'] + ", I'm JugheadBot, your friendly neighbourhood Restaurant finding Bot"
+        fb.independantTextMessage(fb.sender_id, greeting_message)
+        help_message(fb)
+    elif "HELP_TEXT" in postback:
+        help_message(fb)
+    elif "LIKED_JUGHEADBOT" in postback:
+        fb.independantTextMessage(fb.sender_id, "Thanks !! I'll let Raseel know how much you liked me !!")
+    elif "INTERESTED_IN_JUGHEADBOT" in postback:
+        fb.independantTextMessage(fb.sender_id, "Hmm ... I'll try my best to be more helpful and entertaining")
+    elif "BORED_WITH_JUGHEADBOT" in postback:
+        fb.independantTextMessage(fb.sender_id, "That's too bad :-( But hey, don't blame me, Raseel's the one who's made me Boring")
+    else:
+        fb.independantTextMessage(fb.sender_id, "I Live to Eat")
+
+
+def help_message(fb):
+    """
+    Help for JugheadBot
+    """
+    fb.independantTextMessage(fb.sender_id, "Currently, I understand only the following 3 types of questions")
+    sleep(2)
+    fb.independantTextMessage(fb.sender_id, "Best Restaurants : \"Which are the best Restaurants in Kothrud, Pune\"")
+    sleep(2)
+    fb.independantTextMessage(fb.sender_id, "Best Restaurants by Cuisine : \"Which are the best Chinese Restaurants in Dadar, Mumbai\"")
+    sleep(2)
+    fb.independantTextMessage(fb.sender_id, "Reviews for a specific Restaurant : \"What is the review of Blue Nile in Camp Area, Pune\"")
+    sleep(2)
+    fb.independantTextMessage(fb.sender_id, "PLEASE REMEMBER to specify an Area and City. For example : \"Gomti Nagar, Lucknow\" or \"Mahim, Mumbai\" or \"Kothrud, Pune\"")
